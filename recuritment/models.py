f84931b6cfe42cb151d27e0d951533f2ea24c377
kyeon06 <jkyeon06@gmail.com>
2023-10-14 01:19:21 +0900

    feat: Recuritment Model 생성

--- recuritment/models.py
+++ recuritment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from company.models import Company
+
+class Recuritment(models.Model):
+    """
+    기업이 등록한 채용 공고 모델입니다.
+    """
+    position = models.CharField("채용포지션", max_length=50)
+    compensation= models.IntegerField("채용보상금", default=0)
+    contents = models.TextField("채용내용")
+    tech = models.CharField("사용기술", max_length=100)
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
