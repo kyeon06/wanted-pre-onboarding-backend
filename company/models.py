f8b7dbb8bc9e6aa315ab06c79dce2fd82b990bd7
kyeon06 <jkyeon06@gmail.com>
2023-10-14 01:18:55 +0900

    feat: Company Model 생성

--- company/models.py
+++ company/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    """
+    채용공고를 등록할 수 있는 회사 모델입니다.
+    """
+    name = models.CharField("회사명", max_length=50)
+    address = models.CharField("주소", max_length=200)
+    nation = models.CharField("국가", max_length=50)
+    region = models.CharField("지역", max_length=50)
+
+    def __str__(self):
+        return self.name
