2cb192e6efcc42a3fde1abf8c017ed49392bce9a
kyeon06 <jkyeon06@gmail.com>
2023-10-16 21:12:30 +0900

    feat: 채용공고 목록조회/등록 APIView

--- recuritment/views.py
+++ recuritment/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView, CreateAPIView
+from recuritment.models import Recuritment
+from recuritment.serializers import RecuritmentCreateSerializer, RecuritmentListSerializer
 
-# Create your views here.
+""" 채용공고 목록 조회 """
+class RecuritmentListAPIView(ListAPIView):
+    queryset = Recuritment.objects.all()
+    serializer_class = RecuritmentListSerializer
+
+""" 채용공고 등록 """
+class RecuritmentCreateAPIView(CreateAPIView):
+    queryset = Recuritment.objects.all()
+    serializer_class = RecuritmentCreateSerializer
