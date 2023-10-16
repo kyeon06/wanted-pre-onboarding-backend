512bc9960be451b2ec5119277e1232d71a019c2c
kyeon06 <jkyeon06@gmail.com>
2023-10-16 21:12:08 +0900

    feat: 채용공고 목록조회/등록 시리얼라이저 생성

--- recuritment/admin.py
+++ recuritment/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from recuritment.models import Recuritment
+
 # Register your models here.
+admin.site.register(Recuritment)
---
+++ recuritment/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from recuritment.models import Recuritment
+
+""" 채용공고 목록 조회 Serializer """
+class RecuritmentListSerializer(serializers.ModelSerializer):
+    company_name = serializers.CharField(source = 'company.name')
+    nation = serializers.CharField(source = 'company.nation')
+    region = serializers.CharField(source = 'company.region')
+
+    class Meta:
+        model = Recuritment
+        fields = ("id",
+                  "company_name",
+                  "nation",
+                  "region", 
+                  "position",
+                  "compensation",
+                  "tech")
+
+""" 채용공고 등록 Serializer """
+class RecuritmentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recuritment
+        fields = ("company", "position", "compensation", "contents", "tech")
